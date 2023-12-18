4c8b35b0b88047e35569cce3a0251c3b2bd5e6a9
jjyanangomez <juanyasa_04@hotmail.com>
2023-12-18 01:30:50 -0500

    backend y frontedWeb juntos

--- script/consumo.js
+++ script/consumo.js
@@ -1,8 +1,9 @@
-let URL = "http://127.0.0.1:5000/NecesidadesLista";
+let URL = "http://127.0.0.1:5000/";
+//let URL = "http://127.0.0.1:5000/Necesidades";
 
 document.addEventListener("DOMContentLoaded", function () {
     // Realizar la solicitud HTTP para obtener el JSON
-    fetch(URL)
+    fetch(URL+"NecesidadesLista")
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Error al obtener el archivo JSON: ${response.status} ${response.statusText}`);
@@ -12,13 +13,66 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             // Manipular el DOM para mostrar los datos
             const userListElement = document.getElementById("NecesidadesList");
-            data.forEach(nece => {
-                const listItem = document.createElement("li");
-                listItem.textContent = nece.Title;
-                userListElement.appendChild(listItem);
+            Object.keys(data).forEach(nece => {
+                const button  = document.createElement("input");
+                button.type = "button";
+                button.className = "Boton";
+                button.id = `NececidadButton_${nece}`;
+                button.value = data[nece].Title;
+                button.textContent = data[nece].Title;
+                button.addEventListener('click',function() {
+                    llamarInformacion(nece,data[nece].Title);
+                });
+                //button.onclick = llamarInformacion();
+                userListElement.appendChild(button);
             });
         })
         .catch(error => {
             console.error("Error:", error);
         });
 });
+
+function llamarInformacion (documento, name){
+    console.log(documento+name);
+    fetch(URL+'DocNecesidadesInfo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            Document: documento,
+            Name: name
+        })
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`Error de red: ${response.status}`);
+        }
+        return response.json();
+    }).then(data => {
+        // Manejar la información devuelta
+        console.log(data);
+        const resultTitulo = document.getElementById("txtTitulo");
+        resultTitulo.value = data[0]?.Título;
+        const resultDefi = document.getElementById('txtDefinicion');
+        resultDefi.value = data[0]?.Definición;
+        const resultObjetivos = document.getElementById('txtObjetivos');
+        resultObjetivos.value = data[0]?.Objetivo;
+        const resultrAfecciones = document.getElementById('txtAfecciones');
+        resultrAfecciones.value = "";
+        Object.entries(data[0]?.["Afecciones Derivadas"]).forEach(([afeccion, descripcion]) => {
+            console.log(`${afeccion}: ${descripcion}`);
+            resultrAfecciones.value = resultrAfecciones.value+`${afeccion}: ${descripcion}`;
+          });
+        const resultrCuidados = document.getElementById('txtCuidados');
+        resultrCuidados.value = "";
+        Object.entries(data[0]?.["Cuidados por Aplicar"]).forEach(([afeccion, descripcion]) => {
+            console.log(`${afeccion}: ${descripcion}`);
+            resultrCuidados.value = resultrCuidados.value+`${afeccion}: ${descripcion}`;
+          });
+
+    }) .catch(error => {
+        // Manejar errores de la solicitud
+        console.error('Error de solicitud:', error);
+
+    });
+}
